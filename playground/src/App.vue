1a7e3c3515ba511fae124fbc9f7e33db167202e6
Simon he <13917107460@163.com>
2022-06-29 21:34:42 +0800

    chore: update

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -20,6 +20,8 @@ function fn(timestamp) {
   requestAnimationFrame(fn)
 }
 requestAnimationFrame(fn)
+
+const textInput = ref('')
 </script>
 
 <template>
@@ -34,26 +36,36 @@ requestAnimationFrame(fn)
     />
     <dot-text text="Hi,Simon" color="grey" font-size="20" font-weight="5" ma m-y-20 />
     <dot-text :text="date" color="grey" font-size="20" font-weight="5" ma m-y-20 />
-    <dot-text text="China No.1" color="grey" font-size="20" font-weight="5" ma m-y-20 />
+    <dot-text text="China No.1" color="grey" font-size="20" font-weight="5" ma m-y-10 />
     <div w-55 overflow-hidden ma>
       <dot-text
         text="壹 贰 叁 肆 伍 陆 柒 捌 玖 拾 百 千 万 円"
         color="grey"
-        font-size="40"
+        font-size="30"
         font-weight="5"
         ma
-        m-y-20
+        m-y-5
         class="dot-text-wrap"
       />
     </div>
-
+    <input
+      v-model="textInput"
+      type="text"
+      placeholder="尝试输入吧"
+      border-1
+      border-dark
+      border-rd-1
+      indent-2
+      color-black
+    >
+    <dot-text :text="textInput" color="grey" font-size="40" font-weight="5" ma m-y-20 />
     <Footer />
   </main>
 </template>
 
 <style scoped>
 .dot-text-wrap {
-  animation: dot-text-wrap-animation 5s ease-in-out infinite;
+  animation: dot-text-wrap-animation 5s linear infinite;
 }
 @keyframes dot-text-wrap-animation {
   0% {
